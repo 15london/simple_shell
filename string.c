e128bab6704a0f962ebcee91e52e7405d481bbf5
15london <lilpromzy15@gmail.com>
2023-08-25 09:45:23 -0700

    string

--- string.c
+++ string.c
@@ -1,32 +1,30 @@
 #include "shell.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string whose length to check.
+ * _strlen - returns the length of a string
+ * @s: the string whose length to check
  *
- * Return: Integer length of the string.
+ * Return: integer length of string
  */
-
 int _strlen(char *s)
 {
-	int k = 0;
+	int i = 0;
 
 	if (!s)
 		return (0);
 
 	while (*s++)
-		k++;
-	return (k);
+		i++;
+	return (i);
 }
 
 /**
- * _strcmp - Performs lexicographic comparison of two strings.
- * @s1: The first string.
- * @s2: The second string.
+ * _strcmp - performs lexicogarphic comparison of two strangs.
+ * @s1: the first strang
+ * @s2: the second strang
  *
- * Return: Negative if s1 < s2, positive if s1 > s2, zero if s1 == s2.
+ * Return: negative if s1 < s2, positive if s1 > s2, zero if s1 == s2
  */
-
 int _strcmp(char *s1, char *s2)
 {
 	while (*s1 && *s2)
@@ -43,13 +41,12 @@ int _strcmp(char *s1, char *s2)
 }
 
 /**
- * starts_with - Checks if the needle starts with the haystack.
- * @haystack: String to search.
- * @needle: Substring to find.
+ * starts_with - checks if needle starts with haystack
+ * @haystack: string to search
+ * @needle: the substring to find
  *
- * Return: Address of the next character of haystack or NULL.
+ * Return: address of next char of haystack or NULL
  */
-
 char *starts_with(const char *haystack, const char *needle)
 {
 	while (*needle)
@@ -59,13 +56,12 @@ char *starts_with(const char *haystack, const char *needle)
 }
 
 /**
- * _strcat - Concatenates two strings.
- * @dest: The destination buffer.
- * @src: The source buffer.
+ * _strcat - concatenates two strings
+ * @dest: the destination buffer
+ * @src: the source buffer
  *
- * Return: Pointer to the destination buffer.
+ * Return: pointer to destination buffer
  */
-
 char *_strcat(char *dest, char *src)
 {
 	char *ret = dest;
@@ -77,4 +73,3 @@ char *_strcat(char *dest, char *src)
 	*dest = *src;
 	return (ret);
 }
-
